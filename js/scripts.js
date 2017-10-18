7256423a265b1c7a51e04bc035d0ddb3ae413e2a
Kevin Babcock <kevinmbabcock@icloud.com>
2017-10-18 13:38:39 -0700

    add user interface for addition, subtraction, multiplication, and division

--- js/scripts.js
+++ js/scripts.js
@@ -65,7 +65,28 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
-
+    $("#addOutput").text(result);
   });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#subOutput").text(result);
+  })
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = multiply(number1, number2);
+    $("#multiplyOutput").text(result);
+  })
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#divisionOutput").text(result);
+  })
+
 });
